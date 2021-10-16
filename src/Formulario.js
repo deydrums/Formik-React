1974665dad2581b1aeec1566b6e8c66ee86e7102
deydrums <dagarcia100@gmail.com>
2021-10-16 11:17:17 -0600

    Validar nombre

--- src/Formulario.js
+++ src/Formulario.js
@@ -12,13 +12,24 @@ const Formulario = () => {
 				nombre: "David",
 				correo: "dagarcia100@gmail.com"
 			}}
+			validate={(valores)=>{
+				let errores = {};
+
+				if(!valores.nombre){
+					errores.nombre = 'Por favor ingresa un nombre'
+				}else if(!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.nombre)){
+					errores.nombre = 'El nombre solo puede contener letras y espacios'
+				}
+
+				return errores;
+			}}
 			onSubmit={(valores)=>{
 				console.log(valores)
-				console.log("Formulario enviado")
 			}}
 		>
-			{({values, handleSubmit, handleChange,handleBlur}) =>(
+			{({values, errors, handleSubmit, handleChange,handleBlur}) =>(
 			<form className="formulario" onSubmit={handleSubmit}>
+
 				<div>
 					<label htmlFor="nombre">Nombre</label>
 					<input 
@@ -31,6 +42,7 @@ const Formulario = () => {
 					onBlur={handleBlur}
 					/>
 				</div>
+				{errors.nombre && <div className="error">{errors.nombre}</div>}
 				<div>
 					<label htmlFor="correo">Nombre</label>
 					<input 
@@ -43,6 +55,7 @@ const Formulario = () => {
 					onBlur={handleBlur}
 					/>
 				</div>
+				{errors.correo && <div className="error">{errors.correo}</div>}
 				<button type="submit">Enviar</button>
 			</form>
 			)}
